312c38c8a0265c868cecbc237f44a7938048ec0a
teresiahkingori <trizahkingori@gmail.com>
2023-02-17 21:28:11 +0300

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@
  **/
 int main(void)
 {
-	char low;
-	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-	putchar(low);
-	putchar('\n');
-	return (0);
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
 }
