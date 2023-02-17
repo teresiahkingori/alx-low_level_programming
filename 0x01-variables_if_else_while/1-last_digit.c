7b462a37e82152c91f70bd9d74cc647c898f59ae
teresiahkingori <trizahkingori@gmail.com>
2023-02-17 15:17:56 +0300

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  ** main - assign a random number to the variable n each time it is executed
  ** The string Last digit of n
  ** Return: 0
  **/
-
 int main(void)
 {
 int n;
