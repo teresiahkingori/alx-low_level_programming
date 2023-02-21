a70d47cacc14f17401a3788098b05f4d7e10706e
teresiahkingori <trizahkingori@gmail.com>
2023-02-21 21:35:56 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#include "update_putchar.c"
+#include "main.h"
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ ** main - print _putchar
+ ** Return: 0 if successful
+ **/
 int main(void)
 {
-char *sh = "Alx";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
+}
