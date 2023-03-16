a4572038ea4e7a95395fc008a0d82c52a7dadc1f
teresiahkingori <trizahkingori@gmail.com>
2023-03-16 17:49:18 +0300

    function that creates an array of integers.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,35 +1,24 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- **_memset - fills memory with a constant byte
- ** @s: memory area to be filled
- ** @b: char to copy
- ** @n: number of times to copy b
- ** Return: pointer to the memory area s
- **/
-char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
-}
-/*
- **_calloc - allocates memory for an array
- ** @nmemb: number of elements in the array
- ** @size: size of each element
- ** Return: pointer to allocated memory
+ ** _calloc - allocates memory for an array.
+ ** @nmemb: number of elements.
+ ** @size: size of bytes.
+ **
+ ** Return: pointer to the allocated memory.
+ ** if nmemb or size is 0, returns NULL.
+ ** if malloc fails, returns NULL.
  **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr;
+char *p;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
-ptr = malloc(size * nmemb);
-if (ptr == NULL)
+p = malloc(nmemb * size);
+if (p == NULL)
 return (NULL);
-_memset(ptr, 0, nmemb * size);
-return (ptr);
+for (i = 0; i < (nmemb * size); i++)
+p[i] = 0;
+return (p);
 }
