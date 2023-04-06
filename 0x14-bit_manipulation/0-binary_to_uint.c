76ad5c2d246d4b400b86dade0d5041163868c281
teresiahkingori <trizahkingori@gmail.com>
2023-04-06 14:25:42 +0300

    a function that converts a binary number to an unsigned int.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* the code will convert binary number to unsigned int
+* @b: the string that will contain the binary number for this
+* Return: the number that will be converted.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int dec_val = 0;
+if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+dec_val = 2  dec_val + (b[i] - '0');
+}
+return (dec_val);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+0x14. C - Bit manipulation
+Computers represent all data internally as sequences of
+bits.
+Each bit can assume the value 0 or the value 1.
+The bitwise operators are used to manipulate the bits of
+integral operands both signed and unsigned.
+Unsigned integers are normally used with the bitwise
+operators.
+Bitwise manipulations are machine dependent.
