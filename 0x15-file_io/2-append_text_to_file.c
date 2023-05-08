9a0dfede8abfdbc1b6f8bc4be63d4b2c0c577946
teresiahkingori <trizahkingori@gmail.com>
2023-05-08 22:16:42 +0300

    a program that copies the content of a file to another file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- *  *  * append_text_to_file - appends text to
- *   *   * the end of a file
- *    *    * @filename: filename
- *     *     * @text_content: added contect
- *      *      * Return: 1 if the file exists, -1 if it doesn't
- *       *       * or if is a fail
+ * append_text_to_file - appends text to the end of a file;
+ * @filename: filename;
+ * @text_content: added contect;
+ * Return: 1 if the file exists, -1 if it doesn't or if is a fail;
  **/
 int append_text_to_file(const char *filename, char *text_content)
 {
