8a098689ec9ab2402d1c08c140d8e919d9c0a939
teresiahkingori <trizahkingori@gmail.com>
2023-07-06 15:08:49 +0300

    a function that sets the value of a bit to 0 at a given index.

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * flip_bits -this returns the no. of bits needed to flip to get from one no. to another
+ * @n: a first no.
+ * @m: a second no.
+ * Return:the no. of bits.
+ **/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int nbits;
+for (nbits = 0; n || m; n >>= 1, m >>= 1)
+{
+if ((n & 1) != (m & 1))
+nbits++;
+}
+return (nbits);
+}
